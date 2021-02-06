a458366d89b445da10a6d38ddd77125c5a895517
Lyndon <none>
2021-02-06 18:42:25 +0000

    test-change

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Yo! wattup im Donny dev</p>
+        <p>My test development app</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
